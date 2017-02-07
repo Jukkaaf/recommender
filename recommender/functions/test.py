8ce6da81634a4a5646c9c0b94a23a360722a2af8
jukkaaf <jukkaaf@gmail.com>
2017-02-07 21:21:46 +0200

    Testi funktio

---
+++ recommender/functions/test.py
@@ -0,0 +1,2 @@
+def testfunction():
+    return("testi funktion tulos")
--- recommender/views.py
+++ recommender/views.py
@@ -1,6 +1,13 @@
 from pyramid.view import view_config
-
+from functions.test import testfunction
 
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
 def my_view(request):
     return {'project': 'recommender'}
+
+@view_config(route_name='test', renderer='templates/test.jinja2')
+def test(request):
+    result = testfunction()
+    return {'test': 'testi teksti',
+            'result': result
+            }
